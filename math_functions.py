4690aeb00058eb9b198fc16302f094e025a0c991
GuðmundurA <gudmundura20@ru.is>
2020-09-22 00:19:34 +0000

    func b

--- math_functions.py
+++ math_functions.py
@@ -1,12 +1,6 @@
 # Þetta forrit skal hafa föll til stærðfræðilegra reikninga.
 # Forritið skal útbúa valmynd sem prompta notanda fyrir þau föll sem forritið inniheldur
-# Notandi skal geta valið föllin:
-# sum_natural(n_str): , sum_fibonacci(n_str): , approximate_euler(n_str):
-# og sett inn streng n_str(integer) sem gildi breytustærðar fallsins.
-# Enter option: úttakið skal promptast eftir hvert fall sem er notað
-# strengurinn x lokar forritinu, Enter option: x
-# Úttakið "Unrecognized option k" þar sem k er invalid option fyrir þetta forrit
-# 1. Valmynd falla 2a). input notanda, 2b) úttak um villuboð, 3. Túlka input notanda sem integer. Annars villuboð 4. Output.
+# Notandi skal geta valið föllin
 
 def menu():
     '''Represent the functions in the program'''
@@ -26,32 +20,48 @@ def sum_natural(n_str):
         for number in range(1, func_int): # func_int er gefið frá notanda
             number += 1
             number_sum += number
-        return number_sum
+        return number_sum  # return kemur út eftir því hvernig er kallað á fallið
     else:
-        return
+        print('Error:', func_int, 'was not a valid number') # Þegar fallið er kallað
     
-#def sum_fibonacci(n_str)
+def sum_fibonacci(n_str):
+    '''Fibonacci sequence sum'''
+    first_n = 0
+    second_n = 1
+    fibo_sum = 1
+    while func_int >= 2:
+        for n in range(0, func_int-2): 
+            step_value = first_n + second_n 
+            first_n = second_n
+            second_n = step_value # each value after gets the sum of the last two
+            fibo_sum += step_value
+        return fibo_sum
+    else:
+        print('Error:', func_int, 'was not a valid number')
     
 #def approximate_euler(n_str):
-
-#def exit():
-    #return exit()
     
 menu() # Beginning of the program where the functions are represented
 options = 'abc'
 while True:
     n_str = input('Enter option: ')  # input 1
+    
+    if n_str == 'x':
+        break # the whole program ends
     if not n_str in options:
         print("Unrecognized option", n_str)
         menu() # option was not a string that calls a function
         n_str = input('Enter option: ')
-    if n_str == 'x':
-        break # the whole program ends
     
     func_int = int(input('Enter N: ')) # input 2
+    
     if n_str == 'a': # notandi vill sjá fall a.
-        print('Natural number sum: ',sum_natural('a'))
-        continue 
+        print('Natural number sum:',sum_natural('a'))
+        continue
+    elif n_str == 'b': # notandi vill sjá fall b.
+        print('Fibonacci sum:',sum_fibonacci('b'))
+    
+    
     
 
     
